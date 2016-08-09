957777f5a0c53f7358b55fe6276874ef49288913
Estudiante <Estudiante@FI454402-33.unal.loc>
2016-08-09 15:29:20 -0500

    Starting commit

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Test {
+    public static void main(String [] args) {
+        int x=0;
+        int y=0;
+        while (x<5){
+            y=x-y;
+            System.out.print(x+""+y+" ");
+            x=x+1;
+            
+        }
+    }
+    
+}
